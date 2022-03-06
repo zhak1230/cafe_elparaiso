fd5c6202b77ff72029807a141f58f2bca9534145
zhak1230 <zhak1230@gmail.com>
2022-03-06 11:45:18 +0900

    메인페이지 navbar 완성, top버튼 완성

--- main.js
+++ main.js
@@ -100,3 +100,54 @@ document.addEventListener('scroll', () => {
     menuText.classList.remove('text__slide');
   }
 });
+
+// Make navbar transparent when it is on the top
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark');
+  } else {
+    navbar.classList.remove('navbar--dark');
+  }
+});
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  navbarMenu.classList.remove('open');
+  scrollIntoView(link);
+});
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+});
+
+// Back-to-top
+const backToTopBtn = document.querySelector('#back-to-top');
+const backToTopHeight = backToTopBtn.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  // console.log(backToTopHeight); (풀면 로그 보임)
+  // console.log(window.scrollY);
+  if (window.scrollY > backToTopHeight) {
+    backToTopBtn.classList.add('visible');
+  } else {
+    backToTopBtn.classList.remove('visible');
+  }
+});
+
+backToTopBtn.addEventListener('click', () => {
+  scrollIntoView('.swiper');
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
